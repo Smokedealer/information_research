c4f15448a48751a22548ec44b2bc2fa8ebb007f6
Jakub Zíka <j.zika23@gmail.com>
2016-05-15 23:55:04 +0200

    Add ../../nlp/ir/trec

---
+++ interface/src/main/java/cz/zcu/kiv/nlp/ir/trec/Tags.java
@@ -0,0 +1,21 @@
+package cz.zcu.kiv.nlp.ir.trec;
+
+public enum Tags {
+	LANG("lang"),
+    TOPIC("topic"),
+    IDENTIFIER("identifier"),
+    TITLE("title"),
+    DESCRIPTION("description"),
+    NARRATIVE("narrative");
+
+    private String tag;
+
+	Tags(String url) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+}
